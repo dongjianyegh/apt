0437e2408daea0e4faea9e74fe550cdfe1f2c3ee
dongjianyegh <382117107@qq.com>
2019-09-12 18:41:33 +0800

    提交部分代码

--- app/src/main/java/com/example/dongjianye/apt/MainActivity.java
+++ app/src/main/java/com/example/dongjianye/apt/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.dongjianye.apt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.apt.facade.annotation.Route;
+import com.example.apt.launcher.ARouter;
 
-@Route(path = "/test/MainActivity")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ARouter.init(getApplication());
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ARouter.getInstance()
+                        .build("/test/SecondActivity")
+                        .navigation();
+            }
+        });
     }
 }
