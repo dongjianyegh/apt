2ca7982b2757ed52e701214dbe8386120fd34882
dongjianyegh <382117107@qq.com>
2019-09-16 16:31:31 +0800

    提交部分代码

---
+++ apt-gradle-plugin/src/main/groovy/com/example/apt/register/core/RegisterTransform.groovy
@@ -0,0 +1,125 @@
+package com.example.apt.register.core
+
+import com.example.apt.register.utils.Logger
+import com.example.apt.register.utils.ScanSetting
+import com.example.apt.register.utils.ScanUtil
+
+import com.android.build.api.transform.Context
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInput
+import com.android.build.api.transform.TransformInvocation
+import com.android.build.api.transform.TransformOutputProvider
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.example.apt.register.utils.ScanSetting
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.FileUtils
+import org.gradle.api.Project
+
+public class RegisterTransform extends Transform {
+
+    Project project
+    static ArrayList<ScanSetting> registerList
+    static File fileContainsInitClass;
+    public RegisterTransform(Project project) {
+        this.project = project
+    }
+
+    @Override
+    public String getName() {
+        return ScanSetting.PLUGIN_NAME
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs,
+                   Collection<TransformInput> referencedInputs,
+                   TransformOutputProvider outputProvider,
+                   boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        Logger.i('Start scan register info in jar file.')
+
+        long startTime = System.currentTimeMillis()
+        boolean leftSlash = File.separator == '/'
+
+        inputs.each { TransformInput input ->
+
+            input.jarInputs.each { JarInput jarInput ->
+                String destName = jarInput.name
+
+                def hexName = DigestUtils.md5Hex(jarInput.file.absolutePath)
+
+                if (destName.endsWith(".jar")) {
+                    destName = destName.substring(0, destName.length() - 4)
+                }
+
+                File src = jarInput.file;
+                File dest = outputProvider.getContentLocation(destName + "_" + hexName, jarInput.contentTypes, jarInput.scopes, Format.JAR)
+
+                //scan jar file to find classes
+                if (ScanUtil.shouldProcessPreDexJar(src.absolutePath)) {
+                    ScanUtil.scanJar(src, dest)
+                }
+                FileUtils.copyFile(src, dest)
+
+            }
+
+            // scan class files
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                File dest = outputProvider.getContentLocation(directoryInput.name, directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)
+                String root = directoryInput.file.absolutePath
+                if (!root.endsWith(File.separator))
+                    root += File.separator
+                directoryInput.file.eachFileRecurse { File file ->
+                    def path = file.absolutePath.replace(root, '')
+                    if (!leftSlash) {
+                        path = path.replaceAll("\\\\", "/")
+                    }
+                    if(file.isFile() && ScanUtil.shouldProcessClass(path)){
+                        ScanUtil.scanClass(file)
+                    }
+                }
+
+                // copy to dest
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+        }
+
+        Logger.i('Scan finish, current cost time ' + (System.currentTimeMillis() - startTime) + "ms")
+
+        if (fileContainsInitClass) {
+            registerList.each { ext ->
+                Logger.i('Insert register code to file ' + fileContainsInitClass.absolutePath)
+
+                if (ext.classList.isEmpty()) {
+                    Logger.e("No class implements found for interface:" + ext.interfaceName)
+                } else {
+                    ext.classList.each {
+                        Logger.i(it)
+                    }
+                    RegisterCodeGenerator.insertInitCodeTo(ext)
+                }
+            }
+        }
+
+        Logger.i("Generate code finish, current cost time: " + (System.currentTimeMillis() - startTime) + "ms")
+    }
+}
